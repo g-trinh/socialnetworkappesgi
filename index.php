a079712065a9761d6f0cf9e7e7804ef156ba68e3
Guillaume <gutrinh@gmail.com>
2015-04-07 14:34:06 +0200

    modification index

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-test
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Titre de ma page</title>
+		<meta title="description" content="contenu de ma page">
+	</head>
+	<body>
+		Hello
+	</body>
+</html>
